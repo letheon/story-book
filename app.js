cdfe28a1cd2b666b14d0b6ec17dd0e5a65091516
Paul <letheon@gmail.com>
2020-07-30 19:30:07 +0800

    Setup basic main and login layouts

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDb = require('./config/db');
 
+// Load configs
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
 connectDb();
 
+// Create express app
 const app = express();
+
+// Setup middleware
+
+// Add logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Set handlebars as the view engine
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+// Set hosting port
 const PORT = process.env.PORT || 3000;
 
 app.listen(
